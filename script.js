228758092b214169a011f870bf41485e02d23ceb
dayro florez <desing.dayroflorez@outlook.com>
2024-05-21 16:58:35 -0500

    corregir codigo de pokeapi

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 console.log('peticiones')
 
-const obterPokemomon = async () => {
+const obterPokemon = async () => {
     const respuesta = await fetch("https://pokeapi.co/api/v2/pokemon/pikachu")
     const data = await respuesta.json()
     console.log(data)
