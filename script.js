a5ac2d34369912a3023a362eb14e98094bd52f45
dayro florez <desing.dayroflorez@outlook.com>
2024-05-20 21:12:23 -0500

    agregar peticiones a una api

---
+++ script.js
@@ -0,0 +1,9 @@
+console.log('peticiones')
+
+const obterPokemomon = async () => {
+    const respuesta = await fetch("https://pokeapi.co/api/v2/pokemon/pikachu")
+    const data = await respuesta.json()
+    console.log(data)
+}
+
+obterPokemon()
